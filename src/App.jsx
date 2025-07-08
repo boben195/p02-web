fedf2927a57d8953d6a8aacf73e5612eee1e747e
boben195 <boben195@ukr.net>
2025-07-08 18:38:17 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Homepage from "./pages/Homepage/Homepage";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Cart from "./Components/Cart/Cart";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import "./Navbar.css";
+import { BiCart, BiUser } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="navbar">
+        <Link to="/">
+          <div className="logo">
+            <h2>ShopX</h2>
+          </div>
+        </Link>
+        <div className="links">
+          <ul>
+            <li>Home</li>
+            <li>Product</li>
+            <li>Contact</li>
+            <li>App</li>
+          </ul>
+        </div>
+        <div className="nav_icon_wrapper">
+          <Link to="/cart">
+            <div className="nav_cart">
+              <BiCart className="nav_icon" />
+              <p>0</p>
+            </div>
+          </Link>
+          <BiUser className="nav_icon" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
