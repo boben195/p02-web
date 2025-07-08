21a3fed2a34d20b82322c79734b08fac16369a1c
boben195 <boben195@ukr.net>
2025-07-08 20:12:32 +0300

    add hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./Hero.css";
+import hero_img from "../../assets/hero.png";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero_content">
+          <div className="hero_left">
+            <h2>Unlish Premium Sound Quality</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+              repellat libero fuga nulla quos neque, quidem laboriosam, est,
+              saepe nostrum excepturi?
+            </p>
+            <button>Shop the collection</button>
+          </div>
+          <div className="hero right">
+            <img src={hero_img} alt="Some headphones dude" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
