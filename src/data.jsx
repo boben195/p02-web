3596507ddd39270f2523e53adc07a4da421735ec
boben195 <boben195@ukr.net>
2025-07-08 17:24:12 +0300

    add assets and data

---
+++ src/data.jsx
@@ -0,0 +1,123 @@
+import headphone1 from "./assets/head1.jpg";
+import headphone2 from "./assets/head2.jpg";
+import headphone3 from "./assets/head3.jpg";
+import headphone4 from "./assets/head4.jpg";
+import headphone5 from "./assets/head5.jpg";
+import headphone6 from "./assets/head6.jpg";
+import headphone7 from "./assets/head7.jpg";
+import headphone8 from "./assets/head8.jpg";
+import headphone9 from "./assets/head9.jpg";
+import headphone10 from "./assets/head10.jpg";
+import headphone11 from "./assets/head11.jpg";
+import headphone12 from "./assets/head12.jpg";
+
+export const productData = [
+  {
+    id: 1,
+    image: headphone1,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 2,
+    image: headphone2,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 3,
+    image: headphone3,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 4,
+    image: headphone4,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 5,
+    image: headphone5,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 6,
+    image: headphone6,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 7,
+    image: headphone7,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 8,
+    image: headphone8,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 9,
+    image: headphone9,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 10,
+    image: headphone10,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 11,
+    image: headphone11,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+  {
+    id: 12,
+    image: headphone12,
+    title: "Some Phones",
+    price: 45,
+    category: "ladies",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora dolore, eius laborum id velit dolor iure minus dicta, perspiciatis a. Harum soluta quisquam deserunt!",
+  },
+];
