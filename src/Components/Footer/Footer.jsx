ebbde25848148f7de624445ae02b13de00419e31
boben195 <boben195@ukr.net>
2025-07-10 20:38:11 +0300

    add footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { BsInstagram, BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer_content">
+          <div className="footer name">
+            <h2>ShopX</h2>
+          </div>
+          <div className="footer_socials">
+            <FaFacebook className="footer_icon" />
+            <BsInstagram className="footer_icon" />
+            <BsTwitterX className="footer_icon" />
+          </div>
+        </div>
+        <div className="footer-copy">
+          <p>Copyright and bla bla bla</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
